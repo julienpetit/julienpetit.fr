b4a390570388a59209ac5c8f6499ddbf454f364a
Julien PETIT <contact@julienpetit.fr>
2018-01-09 00:08:30 +0100

    Setup google analytics

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import { BrowserRouter, Route } from "react-router-dom";
 import './index.css';
 import "typeface-roboto-mono";
 import App from './App';
+import ReactGA from 'react-ga';
 import registerServiceWorker from './registerServiceWorker';
+
+ReactGA.initialize('UA-44733126-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
     <BrowserRouter>
